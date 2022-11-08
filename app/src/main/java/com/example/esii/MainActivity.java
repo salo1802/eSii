bdf9014c9d19c592a9cfab5a6f698bedd2acea7f
Salomón Sarria <69904705+salo1802@users.noreply.github.com>
2022-11-08 14:54:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/esii/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.esii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login_button);
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.setPermissions(Arrays.asList("email, user_photos"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
