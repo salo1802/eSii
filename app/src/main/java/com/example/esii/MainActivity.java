ff80e48eb6307f545ea52e3bce5ec27b17be47c2
Salomón Sarria <69904705+salo1802@users.noreply.github.com>
2022-11-28 18:44:30 -0500

    Fixed profile pic

--- app/src/main/java/com/example/esii/MainActivity.java
+++ app/src/main/java/com/example/esii/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.esii;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -21,13 +29,17 @@ public class MainActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private LoginButton loginButton;
 
+    ProfilePictureView profile_iv;
+    TextView name_tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         loginButton = findViewById(R.id.login_button);
-
+        profile_iv = findViewById(R.id.profile_iv);
+        name_tv = findViewById(R.id.name);
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -36,7 +48,30 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                if (loginResult != null ){
+                    GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(@Nullable JSONObject jsonObject, @Nullable GraphResponse graphResponse) {
+                            if(jsonObject != null){
+                                try {
+                                    String profileId = jsonObject.getString("id");
+                                    String username = jsonObject.getString("name");
 
+                                    profile_iv.setPresetSize(ProfilePictureView.NORMAL);
+                                    profile_iv.setProfileId(profileId);
+                                    name_tv.setText(username);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }
+                    });
+                    Bundle bundle = new Bundle();
+                    bundle.putString("fields","id,name,email,gender,birthday");
+                    graphRequest.setParameters(bundle);
+                    graphRequest.executeAsync();
+                }
             }
 
             @Override
